eaf887cbfcb008e039ebb05d9d11f397a7d2704c
Konrad Obal <konrad.obal@miquido.com>
2019-08-02 14:51:31 +0200

    fix(aws): Do not use custom provider for version pinning
    Use "required_providers" instead

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
-# Pin the `aws` provider
-# https://www.terraform.io/docs/configuration/providers.html
-# Any non-beta version >= 2.12.0 and < 2.13.0, e.g. 2.12.X
-provider "aws" {
-  version = "~> 2.12.0"
-}
-
-# Terraform
-#--------------------------------------------------------------
 terraform {
   required_version = "~> 0.11.0"
+  required_providers {
+    aws = ">= 2.12.0"
+  }
 }
 
 # Get object aws_vpc by vpc_id
